966ea7b637c6bb2bf01248e616f66da25920e406
jiya-sahu <jiyasahu135@gmail.com>
2024-12-30 13:54:43 +0530

    connected to database

--- Backened/index.js
+++ Backened/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.DB_CONNECT).then(()=>{
+    console.log("connected to db");
+    
+}).catch((err)=>{
+    console.log(err);
+    
+});
 app.listen(3000,()=>{
     console.log("server started at port 3000");
     
