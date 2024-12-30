9ccb9b1c85a20a6eacf1b2aeb90efca843ca58eb
jiya-sahu <jiyasahu135@gmail.com>
2024-12-30 13:48:56 +0530

    basic server setup

--- Backened/index.js
+++ Backened/index.js
@@ -1,25 +1,7 @@
 import express from 'express'
 const app = express();
-<<<<<<< HEAD
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-mongoose.connect(process.env.DB_CONNECT).then(()=>{
-    console.log("connected to db");
-    
-}).catch((err)=>{
-    console.log(err);
-    
-})
-=======
->>>>>>> 6c036f0565dc4e90450cd8337021d228bfc03436
 
 app.listen(3000,()=>{
-    console.log("server listening at port 3000");
+    console.log("server started at port 3000");
     
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 6c036f0565dc4e90450cd8337021d228bfc03436
