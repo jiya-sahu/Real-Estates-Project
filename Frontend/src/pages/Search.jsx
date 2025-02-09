44c338f5204f2f8276dde1a1eb024262c1b2abad
jiya-sahu <jiyasahu135@gmail.com>
2025-02-10 00:16:41 +0530

    added show more listings  functionality

--- Frontend/src/pages/Search.jsx
+++ Frontend/src/pages/Search.jsx
@@ -112,6 +112,22 @@ function Search() {
     navigate(`/search?${searchUrl}`);
   };
 
+  const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex',startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+       
+        setListings([...listings,...data]);
+  }
+
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -236,6 +252,12 @@ function Search() {
             listings &&
             listings.map((listing) => 
               <ListingItem key={listing._id} listing={listing} />
+            )}
+
+            {showMore && (
+              <button onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">
+                Show More
+              </button>
             )}
         </div>
       </div>
